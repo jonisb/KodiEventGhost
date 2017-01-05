2efb9c414725c541659bb3b0ec4bdadce94b7695
jonisb <github.com@JsBComputing.se>
2017-01-05 15:31:19 +0100

    Added SSDP scanning to plugin configuration

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from __future__ import print_function, unicode_literals, division, absolute_impo
 import eg
 
 import KodiLib
+import SSDPLib # ToDo: Move after eg.RegisterPlugin()?
 
 import logging  # TODO: Move after eg.RegisterPlugin()?
 
@@ -56,6 +57,9 @@ except ImportError:
         raise
 logger.info('Importing "OrderedDict" module, done.')
 
+import types
+import threading
+
 
 def DefaultSettings(Data=None):
     """ """  # TODO
@@ -100,6 +104,140 @@ class Kodi(eg.PluginClass):
     def Configure(self, pluginSettings=None):
         """ """  # TODO
         pluginSettings = DefaultSettings(pluginSettings)
+
+        myEVT_SSDPResult = wx.NewEventType()
+        EVT_SSDPResult = wx.PyEventBinder(myEVT_SSDPResult, 1)
+        class SSDPResultEvent(wx.PyCommandEvent):
+            """Event to signal that a count value is ready"""
+            def __init__(self, etype, eid, value=None):
+                """Creates the event object"""
+                wx.PyCommandEvent.__init__(self, etype, eid)
+                self._value = value
+
+            def GetValue(self):
+                """Returns the value from the event.
+                @return: the value of this event
+
+                """
+                return self._value
+
+        class SSDPScanThread(threading.Thread):
+            def __init__(self, parent, clients):
+                threading.Thread.__init__(self)
+                self._parent = parent
+                self.clients = clients
+
+            def run(self):
+                logger.info('Thread runs')
+                for client in SSDPLib.ssdpSearchGen(self.clients):
+                    logger.info('Create event')
+                    evt = SSDPResultEvent(myEVT_SSDPResult, -1, client)
+                    logger.info('Post event')
+                    wx.PostEvent(self._parent, evt)
+                logger.info('Thread ends')
+
+        class SSDPSearchDialog(wx.Dialog):
+            def __init__(self, parent, title):
+                super(SSDPSearchDialog, self).__init__(parent, title = title, size = (500,150))
+                self.list_ctrl_1 = wx.ListCtrl(self, wx.ID_ANY, style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.SUNKEN_BORDER)
+                self.list_ctrl_1.InsertColumn(0, 'FriendlyName')
+                self.list_ctrl_1.InsertColumn(1, 'Name')
+                self.list_ctrl_1.InsertColumn(2, 'version')
+                self.list_ctrl_1.InsertColumn(3, 'ip')
+                self.list_ctrl_1.InsertColumn(4, 'port')
+                self.list_ctrl_1.InsertColumn(5, 'id')
+                self.index = 0
+                self.ListCtrlData = {}
+
+                self.okBtn = wx.Button(self, wx.ID_OK)
+                self.okBtn.Disable()
+                self.cancelBtn = wx.Button(self, wx.ID_CANCEL)
+                self.rescanBtn = wx.Button(self, wx.ID_REFRESH)
+
+                worker = SSDPScanThread(self, ('Kodi', 'XBMC Media Center'))
+                logger.info('worker starts.')
+                worker.start()
+                # Attributes
+#                worker = CountingThread(self, 1)
+#                logger.info('worker starts.')
+#                worker.start()
+                #self.CreateStatusBar()
+
+                # Layout
+                self.__DoLayout()
+
+                # Event Handlers
+                self.Bind(EVT_SSDPResult, self.AddToListEvent)
+                self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.ActivateListEvent)
+                self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.ITEM_SELECTED)
+                self.Bind(wx.EVT_LIST_ITEM_DESELECTED, self.ITEM_DESELECTED)
+                self.rescanBtn.Bind(wx.EVT_BUTTON, self.OnButton)
+
+            def __DoLayout(self):
+                Button1sizer = wx.BoxSizer(wx.HORIZONTAL)
+                Button1sizer.Add(self.rescanBtn, 0)
+                Button2sizer = wx.BoxSizer(wx.HORIZONTAL)
+                Button2sizer.Add(self.okBtn, 0)
+                Button2sizer.Add(self.cancelBtn, 0)
+
+                Buttonsizer = wx.BoxSizer(wx.HORIZONTAL)
+                Buttonsizer.Add(Button1sizer, 1, wx.EXPAND)
+                Buttonsizer.Add(Button2sizer, 0)
+
+                sizer = wx.BoxSizer(wx.VERTICAL)
+                sizer.Add(self.list_ctrl_1, 1, wx.EXPAND)
+                sizer.Add(Buttonsizer, 0, wx.EXPAND)
+                self.SetSizer(sizer)
+
+            def OnButton(self, evt):
+                eg.PrintError("ReScan not implemented")
+
+            def ActivateListEvent(self, evt):
+                self.EndModal(wx.ID_OK)
+
+            def ITEM_SELECTED(self, evt):
+                self.okBtn.Enable()
+
+            def ITEM_DESELECTED(self, evt):
+                self.okBtn.Disable()
+
+            def AddToListEvent(self, evt):
+                self.list_ctrl_1.InsertStringItem(self.index, evt.GetValue()['friendlyName'])
+                for i, label in enumerate(('name', 'version', 'ip', 'port', 'id'), 1):
+                    self.list_ctrl_1.SetStringItem(self.index, i, evt.GetValue()[label])
+                self.ListCtrlData[self.index] = evt.GetValue()
+                self.list_ctrl_1.SetItemData(self.index, self.index)
+                self.index += 1
+
+        def OnButton(self, evt):
+            logger.info('OnButton starts.')
+            SSDPSearch = SSDPSearchDialog(self, "Searching for clients")
+            res = SSDPSearch.ShowModal()
+            if res == wx.ID_OK:
+                value = SSDPSearch.list_ctrl_1.GetFirstSelected()
+                self.Settings['server']['network']['address'] = SSDPSearch.ListCtrlData[value]['ip']
+                self.Settings['server']['network']['http']['port'] = SSDPSearch.ListCtrlData[value]['port']
+                self.Settings['server']['friendlyName'] = SSDPSearch.ListCtrlData[value]['friendlyName']
+                self.Settings['server']['name'] = SSDPSearch.ListCtrlData[value]['name']
+                self.Settings['server']['version'] = SSDPSearch.ListCtrlData[value]['version']
+                self.Settings['server']['network']['upnp']['id'] = SSDPSearch.ListCtrlData[value]['id']
+            SSDPSearch.Destroy()
+            logger.info('OnButton ends.')
+
+        def initPanel(self):
+            self.button = wx.Button(self, wx.ID_ANY, "Scan")
+
+            setPanelProperties(self)
+            doPanelLayout(self)
+
+            # Events
+            self.button.Bind(wx.EVT_BUTTON, self.OnButton)
+
+        def setPanelProperties(self):
+            pass
+
+        def doPanelLayout(self):
+            self.sizer.Add(self.button, 0, wx.ALIGN_CENTER, 0)
 
         class test(object):
             def __init__(self, Item):
@@ -170,21 +308,10 @@ class Kodi(eg.PluginClass):
                     self.SetItemHasChildren(item)
                     self.__collapsing = False
 
-        def initPanel(self):
-            setPanelProperties(self)
-            doPanelLayout(self)
-
-        def setPanelProperties(self):
-            pass
-#
-        def doPanelLayout(self):
-            #self.sizer.Add(self.button, 1, wx.ALIGN_CENTER, 0)
-            pass
-
         def initPage(self):
             panel.dialog.notebook.AddPage(self, "Advanced")
             self.Settings = pluginSettings
-            self.tree = LazyTree(self, style=wx.TR_HIDE_ROOT+wx.TR_LINES_AT_ROOT+wx.TR_HAS_BUTTONS+wx.TR_EDIT_LABELS)
+            self.tree = LazyTree(self, style=wx.TR_HIDE_ROOT+wx.TR_LINES_AT_ROOT+wx.TR_HAS_BUTTONS)  # +wx.TR_EDIT_LABELS
 
             setPageProperties(self)
             doPageLayout(self)
@@ -198,7 +325,10 @@ class Kodi(eg.PluginClass):
             self.SetSizer(advanced_sizer)
 
         panel = eg.ConfigPanel()
+        panel.OnButton = types.MethodType(OnButton, panel)
+        panel.Settings = pluginSettings
         initPanel(panel)
+
         panel.AdvancedPanel = wx.Panel(panel.dialog.notebook)
         initPage(panel.AdvancedPanel)
 
